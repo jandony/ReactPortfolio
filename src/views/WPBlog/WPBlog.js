d6aa485c85a10148ef69f82ec448f2e373b7644a
Jeff Andony <jeff.andony@gmail.com>
2020-05-27 09:06:20 -0600

    Uploading React template files.

---
+++ src/views/WPBlog/WPBlog.js
@@ -0,0 +1,104 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const styles = {
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  },
+  media: {
+    paddingTop: '100%', // 16:9 - 56.25% 1:1 - 100%
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  cardActions: {
+    justifyContent: 'flex-start',
+  }
+};
+
+const useStyles = makeStyles(styles);
+
+export default function UserProfile() {
+  const classes = useStyles();
+  return (
+    <div>
+      <GridContainer>
+    
+        <GridItem xs={12} lg={6}>
+            <Card>
+                <GridContainer>
+                    <GridItem xs={12} sm={6}>
+                        <CardMedia
+                            className={classes.media}
+                            image="https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib"
+                            title="Paella dish"
+                        />
+                    </GridItem>
+                    <GridItem xs={12} sm={6}>
+                        <CardHeader
+                            avatar={
+                                <Avatar aria-label="recipe" className={classes.avatar}>
+                                    R
+                                </Avatar>
+                            }
+                            action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                            }
+                            title="Shrimp and Chorizo Paella"
+                            subheader="September 14, 2016"
+                        />
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                            This impressive paella is a perfect party dish and a fun meal to cook together with your
+                            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                            </Typography>
+                        </CardContent>
+                        <CardActions className={classes.cardActions}>
+                                <IconButton aria-label="add to favorites">
+                                    <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                    <ShareIcon />
+                                </IconButton>
+                        </CardActions>
+                    </GridItem>
+                </GridContainer>
+            </Card>
+        </GridItem>
+
+      </GridContainer>
+    </div>
+  );
+}
